c4751ffac4141d635dabde37c27f053b0c77dbed
ranamiah221 <ranarasul21@gmail.com>
2023-09-14 12:37:07 +0600

    set react knowledge projecta

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-bold text-red-500 bg-amber-300'>React Knowledge Cafe</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
